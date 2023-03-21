339e0e901b27bd3d16a40f4f8b7d5c58bf9309ab
Gaius <gaius.qi@gmail.com>
2023-03-21 21:21:08 +0800

    feat: add definition of trainer service (#99)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 "proto/dfdaemon.proto",
                 "proto/manager.proto",
                 "proto/scheduler.proto",
+                "proto/trainer.proto",
             ],
             &["proto/"],
         )?;
