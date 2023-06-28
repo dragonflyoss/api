f94bf8a8e5e89dbf53d74bb74c272b0832b043eb
Gaius <gaius.qi@gmail.com>
2023-06-28 18:57:49 +0800

    feat: add inference proto
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hack/protoc.sh
+++ hack/protoc.sh
@@ -6,7 +6,7 @@ LANGUAGE=go
 
 proto_modules="common/v1 common/v2 cdnsystem/v1 dfdaemon/v1 dfdaemon/v2
 errordetails/v1 manager/v1 manager/v2 scheduler/v1 scheduler/v2
-security/v1 trainer/v1"
+security/v1 trainer/v1 inference/v1"
 
 echo "generate protos..."
 
