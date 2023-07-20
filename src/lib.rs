ad7b5e87a659570be1981c6911fe23b45c6cb7b8
Gaius <gaius.qi@gmail.com>
2023-07-20 15:13:16 +0800

    feat: generate grpc descriptor
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,6 @@ pub mod dfdaemon;
 pub mod manager;
 pub mod scheduler;
 pub mod security;
+
+// FILE_DESCRIPTOR_SET is the serialized FileDescriptorSet of the proto files.
+pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("descriptor.bin");
