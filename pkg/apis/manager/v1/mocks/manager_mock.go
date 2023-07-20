ab0c613b95f018bdd4f776ce175048c123ace569
Gaius <gaius.qi@gmail.com>
2023-07-20 22:05:07 +0800

    feat: generate golang mock (#148)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/manager/v1/mocks/manager_mock.go
+++ pkg/apis/manager/v1/mocks/manager_mock.go
@@ -8,7 +8,7 @@ import (
 	context "context"
 	reflect "reflect"
 
-	manager "d7y.io/api/pkg/apis/manager/v1"
+	manager "d7y.io/api/v2/pkg/apis/manager/v1"
 	gomock "github.com/golang/mock/gomock"
 	grpc "google.golang.org/grpc"
 	metadata "google.golang.org/grpc/metadata"
