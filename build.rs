c1585bdf2b656d15887dc9d8456e2100d7abaed2
Gaius <gaius.qi@gmail.com>
2023-07-27 15:02:53 +0800

    chore: update tonic and tokio version
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- build.rs
+++ build.rs
@@ -1,10 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .file_descriptor_set_path("src/descriptor.bin")
-        .build_client(true)
-        .build_server(true)
-        .out_dir("src")
         .protoc_arg("--experimental_allow_proto3_optional")
+        .out_dir("src")
         .compile(
             &[
                 "proto/common.proto",
