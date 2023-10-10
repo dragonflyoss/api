9e68b1fdb9081cf231e58b805e5c2daa00394679
Gaius <gaius.qi@gmail.com>
2023-10-10 15:15:10 +0800

    feat: add InterestedAllPiecesRequest message
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
+++ pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
@@ -463,6 +463,158 @@ var _ interface {
 	ErrorName() string
 } = SyncPiecesRequestValidationError{}
 
+// Validate checks the field values on InterestedAllPiecesResponse with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *InterestedAllPiecesResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on InterestedAllPiecesResponse with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// InterestedAllPiecesResponseMultiError, or nil if none found.
+func (m *InterestedAllPiecesResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *InterestedAllPiecesResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(m.GetPieces()) > 0 {
+
+		if len(m.GetPieces()) < 1 {
+			err := InterestedAllPiecesResponseValidationError{
+				field:  "Pieces",
+				reason: "value must contain at least 1 item(s)",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		for idx, item := range m.GetPieces() {
+			_, _ = idx, item
+
+			if all {
+				switch v := interface{}(item).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, InterestedAllPiecesResponseValidationError{
+							field:  fmt.Sprintf("Pieces[%v]", idx),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, InterestedAllPiecesResponseValidationError{
+							field:  fmt.Sprintf("Pieces[%v]", idx),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return InterestedAllPiecesResponseValidationError{
+						field:  fmt.Sprintf("Pieces[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return InterestedAllPiecesResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// InterestedAllPiecesResponseMultiError is an error wrapping multiple
+// validation errors returned by InterestedAllPiecesResponse.ValidateAll() if
+// the designated constraints aren't met.
+type InterestedAllPiecesResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m InterestedAllPiecesResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m InterestedAllPiecesResponseMultiError) AllErrors() []error { return m }
+
+// InterestedAllPiecesResponseValidationError is the validation error returned
+// by InterestedAllPiecesResponse.Validate if the designated constraints
+// aren't met.
+type InterestedAllPiecesResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e InterestedAllPiecesResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e InterestedAllPiecesResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e InterestedAllPiecesResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e InterestedAllPiecesResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e InterestedAllPiecesResponseValidationError) ErrorName() string {
+	return "InterestedAllPiecesResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e InterestedAllPiecesResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sInterestedAllPiecesResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = InterestedAllPiecesResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = InterestedAllPiecesResponseValidationError{}
+
 // Validate checks the field values on InterestedPiecesResponse with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -638,6 +790,48 @@ func (m *SyncPiecesResponse) validate(all bool) error {
 
 	oneofResponsePresent := false
 	switch v := m.Response.(type) {
+	case *SyncPiecesResponse_InterestedAllPiecesResponse:
+		if v == nil {
+			err := SyncPiecesResponseValidationError{
+				field:  "Response",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+		oneofResponsePresent = true
+
+		if all {
+			switch v := interface{}(m.GetInterestedAllPiecesResponse()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SyncPiecesResponseValidationError{
+						field:  "InterestedAllPiecesResponse",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SyncPiecesResponseValidationError{
+						field:  "InterestedAllPiecesResponse",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetInterestedAllPiecesResponse()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SyncPiecesResponseValidationError{
+					field:  "InterestedAllPiecesResponse",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	case *SyncPiecesResponse_InterestedPiecesResponse:
 		if v == nil {
 			err := SyncPiecesResponseValidationError{
