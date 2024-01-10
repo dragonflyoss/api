ce44d9501db00536a812d5949f7d5a2e3c118381
Gaius <gaius.qi@gmail.com>
2024-01-10 10:40:57 +0800

    feat: change candidate_parent_ids to candidate_parents (#246)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/scheduler/v2/scheduler.pb.validate.go
+++ pkg/apis/scheduler/v2/scheduler.pb.validate.go
@@ -422,6 +422,40 @@ func (m *RescheduleRequest) validate(all bool) error {
 
 	var errors []error
 
+	for idx, item := range m.GetCandidateParents() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, RescheduleRequestValidationError{
+						field:  fmt.Sprintf("CandidateParents[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, RescheduleRequestValidationError{
+						field:  fmt.Sprintf("CandidateParents[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RescheduleRequestValidationError{
+					field:  fmt.Sprintf("CandidateParents[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if m.Description != nil {
 
 		if utf8.RuneCountInString(m.GetDescription()) < 1 {
