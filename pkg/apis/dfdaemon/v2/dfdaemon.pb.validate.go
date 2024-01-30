e1ef283f1d2a9a2876b0b87d2faf81ffc5d640ee
Gaius <gaius.qi@gmail.com>
2024-01-30 14:09:23 +0800

    feat: add interested piece metadates to DownloadTaskStartedResponse
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
+++ pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
@@ -834,6 +834,40 @@ func (m *DownloadTaskStartedResponse) validate(all bool) error {
 
 	// no validation rules for ResponseHeader
 
+	for idx, item := range m.GetPieces() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, DownloadTaskStartedResponseValidationError{
+						field:  fmt.Sprintf("Pieces[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, DownloadTaskStartedResponseValidationError{
+						field:  fmt.Sprintf("Pieces[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return DownloadTaskStartedResponseValidationError{
+					field:  fmt.Sprintf("Pieces[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if m.Range != nil {
 
 		if all {
