7431d58884aa27634aecc1e7e06f6fe712e77283
Chongzhi Deng <57183262+BruceAko@users.noreply.github.com>
2024-06-28 18:42:37 +0800

    feat: add interval field to AnnounceHostRequest (#330)
    Signed-off-by: BruceAko <chongzhi@hust.edu.cn>

--- pkg/apis/scheduler/v2/scheduler.pb.validate.go
+++ pkg/apis/scheduler/v2/scheduler.pb.validate.go
@@ -3431,6 +3431,39 @@ func (m *AnnounceHostRequest) validate(all bool) error {
 		}
 	}
 
+	if m.Interval != nil {
+
+		if all {
+			switch v := interface{}(m.GetInterval()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, AnnounceHostRequestValidationError{
+						field:  "Interval",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, AnnounceHostRequestValidationError{
+						field:  "Interval",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetInterval()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return AnnounceHostRequestValidationError{
+					field:  "Interval",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return AnnounceHostRequestMultiError(errors)
 	}
