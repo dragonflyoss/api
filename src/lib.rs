8eb0ff01c61489fa6c6c4aaf1df89f6fb30d3865
Gaius <gaius.qi@gmail.com>
2024-10-10 17:32:58 +0800

    feat: remove security proto
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,5 @@ pub mod scheduler {
     pub mod v2;
 }
 
-pub mod security;
-
 // FILE_DESCRIPTOR_SET is the serialized FileDescriptorSet of the proto files.
 pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("descriptor.bin");
