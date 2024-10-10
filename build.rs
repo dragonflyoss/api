50b7abc20eefbaf489e82de7e9780393e0e2c482
Gaius <gaius.qi@gmail.com>
2024-10-10 17:54:09 +0800

    feat: remove security proto (#403)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .file_descriptor_set_path("src/descriptor.bin")
         .protoc_arg("--experimental_allow_proto3_optional")
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]", )
-        .type_attribute("scheduler.v2.AnnouncePeerRequest.request", "#[allow(clippy::large_enum_variant)]", )
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute(
+            "scheduler.v2.AnnouncePeerRequest.request",
+            "#[allow(clippy::large_enum_variant)]",
+        )
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
         .out_dir("src")
@@ -11,7 +14,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &[
                 "proto/common.proto",
                 "proto/errordetails.proto",
-                "proto/security.proto",
                 "proto/dfdaemon.proto",
                 "proto/manager.proto",
                 "proto/scheduler.proto",
