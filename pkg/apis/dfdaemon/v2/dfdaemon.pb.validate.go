44aadfd8a75e30216cc9c2f341861b7e084561bb
baowj <bwj_678@qq.com>
2024-12-31 14:28:36 +0800

    add golang SyncHost method to DfdaemonUpload service
    Signed-off-by: baowj <bwj_678@qq.com>

--- pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
+++ pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
@@ -2550,3 +2550,125 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = DeletePersistentCacheTaskRequestValidationError{}
+
+// Validate checks the field values on SyncHostRequest with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *SyncHostRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on SyncHostRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// SyncHostRequestMultiError, or nil if none found.
+func (m *SyncHostRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *SyncHostRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if utf8.RuneCountInString(m.GetHostId()) < 1 {
+		err := SyncHostRequestValidationError{
+			field:  "HostId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetPeerId()) < 1 {
+		err := SyncHostRequestValidationError{
+			field:  "PeerId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return SyncHostRequestMultiError(errors)
+	}
+
+	return nil
+}
+
+// SyncHostRequestMultiError is an error wrapping multiple validation errors
+// returned by SyncHostRequest.ValidateAll() if the designated constraints
+// aren't met.
+type SyncHostRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SyncHostRequestMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SyncHostRequestMultiError) AllErrors() []error { return m }
+
+// SyncHostRequestValidationError is the validation error returned by
+// SyncHostRequest.Validate if the designated constraints aren't met.
+type SyncHostRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SyncHostRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SyncHostRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SyncHostRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SyncHostRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SyncHostRequestValidationError) ErrorName() string { return "SyncHostRequestValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SyncHostRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSyncHostRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SyncHostRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SyncHostRequestValidationError{}
