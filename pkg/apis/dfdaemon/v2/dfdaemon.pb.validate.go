86b994e4ae8c405471fbcde72f4aab76aab6a2ee
Gaius <gaius.qi@gmail.com>
2025-01-17 15:27:13 +0800

    feat: remove timeout from PersistentCacheTask (#444)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
+++ pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
@@ -3490,6 +3490,39 @@ func (m *UploadPersistentCacheTaskRequest) validate(all bool) error {
 		// no validation rules for Application
 	}
 
+	if m.Timeout != nil {
+
+		if all {
+			switch v := interface{}(m.GetTimeout()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, UploadPersistentCacheTaskRequestValidationError{
+						field:  "Timeout",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, UploadPersistentCacheTaskRequestValidationError{
+						field:  "Timeout",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetTimeout()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return UploadPersistentCacheTaskRequestValidationError{
+					field:  "Timeout",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return UploadPersistentCacheTaskRequestMultiError(errors)
 	}
