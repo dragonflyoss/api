f961661f501cb81d30dbd623abbb5b62b61196a4
Yang Kaiyong <yangkaiyong.yky@antgroup.com>
2025-11-20 14:16:51 +0800

    fix: move auto generated codes to output
    Signed-off-by: Yang Kaiyong <yangkaiyong.yky@antgroup.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let descriptor_path = out_dir.join("descriptor.bin");
+
     tonic_build::configure()
-        .file_descriptor_set_path("src/descriptor.bin")
+        .file_descriptor_set_path(descriptor_path)
         .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute(
@@ -13,7 +19,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
-        .out_dir("src")
         .compile(
             &[
                 "proto/common.proto",
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,32 @@
-#[path = ""]
 pub mod common {
-    #[path = "common.v2.rs"]
-    pub mod v2;
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/common.v2.rs"));
+    }
 }
 
-#[path = ""]
 pub mod errordetails {
-    #[path = "errordetails.v2.rs"]
-    pub mod v2;
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/errordetails.v2.rs"));
+    }
 }
 
-#[path = ""]
 pub mod dfdaemon {
-    #[path = "dfdaemon.v2.rs"]
-    pub mod v2;
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/dfdaemon.v2.rs"));
+    }
 }
 
-#[path = ""]
 pub mod manager {
-    #[path = "manager.v2.rs"]
-    pub mod v2;
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/manager.v2.rs"));
+    }
 }
 
-#[path = ""]
 pub mod scheduler {
-    #[path = "scheduler.v2.rs"]
-    pub mod v2;
+    pub mod v2 {
+        include!(concat!(env!("OUT_DIR"), "/scheduler.v2.rs"));
+    }
 }
 
 // FILE_DESCRIPTOR_SET is the serialized FileDescriptorSet of the proto files.
-pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("descriptor.bin");
+pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/descriptor.bin"));
