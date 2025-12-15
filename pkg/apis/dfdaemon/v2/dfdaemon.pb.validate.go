f9840bbbe37d1c06a7290d819a777914ac6fa2a8
Gaius <gaius.qi@gmail.com>
2025-12-15 10:48:01 +0800

    feat: add StatLocalTaskResponse to dfdaemon
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
+++ pkg/apis/dfdaemon/v2/dfdaemon.pb.validate.go
@@ -1580,6 +1580,267 @@ var _ interface {
 	ErrorName() string
 } = StatLocalTaskRequestValidationError{}
 
+// Validate checks the field values on StatLocalTaskResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *StatLocalTaskResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on StatLocalTaskResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// StatLocalTaskResponseMultiError, or nil if none found.
+func (m *StatLocalTaskResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *StatLocalTaskResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if utf8.RuneCountInString(m.GetTaskId()) < 1 {
+		err := StatLocalTaskResponseValidationError{
+			field:  "TaskId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	// no validation rules for ContentLength
+
+	// no validation rules for ResponseHeader
+
+	// no validation rules for UploadingCount
+
+	// no validation rules for UploadedCount
+
+	if m.GetCreatedAt() == nil {
+		err := StatLocalTaskResponseValidationError{
+			field:  "CreatedAt",
+			reason: "value is required",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if m.GetUpdatedAt() == nil {
+		err := StatLocalTaskResponseValidationError{
+			field:  "UpdatedAt",
+			reason: "value is required",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if m.PieceLength != nil {
+
+		if m.GetPieceLength() != 0 {
+
+			if m.GetPieceLength() < 4194304 {
+				err := StatLocalTaskResponseValidationError{
+					field:  "PieceLength",
+					reason: "value must be greater than or equal to 4194304",
+				}
+				if !all {
+					return err
+				}
+				errors = append(errors, err)
+			}
+
+		}
+
+	}
+
+	if m.PrefetchedAt != nil {
+
+		if all {
+			switch v := interface{}(m.GetPrefetchedAt()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "PrefetchedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "PrefetchedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetPrefetchedAt()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return StatLocalTaskResponseValidationError{
+					field:  "PrefetchedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if m.FailedAt != nil {
+
+		if all {
+			switch v := interface{}(m.GetFailedAt()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "FailedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "FailedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetFailedAt()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return StatLocalTaskResponseValidationError{
+					field:  "FailedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if m.FinishedAt != nil {
+
+		if all {
+			switch v := interface{}(m.GetFinishedAt()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "FinishedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, StatLocalTaskResponseValidationError{
+						field:  "FinishedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetFinishedAt()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return StatLocalTaskResponseValidationError{
+					field:  "FinishedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return StatLocalTaskResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// StatLocalTaskResponseMultiError is an error wrapping multiple validation
+// errors returned by StatLocalTaskResponse.ValidateAll() if the designated
+// constraints aren't met.
+type StatLocalTaskResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m StatLocalTaskResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m StatLocalTaskResponseMultiError) AllErrors() []error { return m }
+
+// StatLocalTaskResponseValidationError is the validation error returned by
+// StatLocalTaskResponse.Validate if the designated constraints aren't met.
+type StatLocalTaskResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e StatLocalTaskResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e StatLocalTaskResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e StatLocalTaskResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e StatLocalTaskResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e StatLocalTaskResponseValidationError) ErrorName() string {
+	return "StatLocalTaskResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e StatLocalTaskResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sStatLocalTaskResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = StatLocalTaskResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = StatLocalTaskResponseValidationError{}
+
 // Validate checks the field values on ListTaskEntriesRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
