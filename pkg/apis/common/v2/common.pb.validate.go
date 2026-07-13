89cca9351bbcd3ab54e1c0ccca7930d6d1dc2052
Gaius <gaius.qi@gmail.com>
2026-07-13 20:42:44 +0800

    feat(common): add CgroupDisk struct and cgroup field to Disk message
    - Add `CgroupDisk` message with `read_bandwidth` and `write_bandwidth`
      fields
    - Add optional `cgroup` field (field 11) to `Disk` message
    - Update message type indices for `Build`, `Download`, `ObjectStorage`,
      and other subsequent messages
    - Bump crate version to 2.2.31
    
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- pkg/apis/common/v2/common.pb.validate.go
+++ pkg/apis/common/v2/common.pb.validate.go
@@ -3372,6 +3372,39 @@ func (m *Disk) validate(all bool) error {
 
 	// no validation rules for WriteBandwidth
 
+	if m.Cgroup != nil {
+
+		if all {
+			switch v := interface{}(m.GetCgroup()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, DiskValidationError{
+						field:  "Cgroup",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, DiskValidationError{
+						field:  "Cgroup",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetCgroup()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return DiskValidationError{
+					field:  "Cgroup",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return DiskMultiError(errors)
 	}
@@ -3448,6 +3481,109 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = DiskValidationError{}
+
+// Validate checks the field values on CgroupDisk with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *CgroupDisk) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on CgroupDisk with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in CgroupDiskMultiError, or
+// nil if none found.
+func (m *CgroupDisk) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *CgroupDisk) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for ReadBandwidth
+
+	// no validation rules for WriteBandwidth
+
+	if len(errors) > 0 {
+		return CgroupDiskMultiError(errors)
+	}
+
+	return nil
+}
+
+// CgroupDiskMultiError is an error wrapping multiple validation errors
+// returned by CgroupDisk.ValidateAll() if the designated constraints aren't met.
+type CgroupDiskMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m CgroupDiskMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m CgroupDiskMultiError) AllErrors() []error { return m }
+
+// CgroupDiskValidationError is the validation error returned by
+// CgroupDisk.Validate if the designated constraints aren't met.
+type CgroupDiskValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e CgroupDiskValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e CgroupDiskValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e CgroupDiskValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e CgroupDiskValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e CgroupDiskValidationError) ErrorName() string { return "CgroupDiskValidationError" }
+
+// Error satisfies the builtin error interface
+func (e CgroupDiskValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sCgroupDisk.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = CgroupDiskValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = CgroupDiskValidationError{}
 
 // Validate checks the field values on Build with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
